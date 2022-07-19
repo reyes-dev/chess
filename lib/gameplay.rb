21c13fc3332cff139381c03121bbb7f0c8aa3565
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-18 23:38:55 -0700

    Add team switching

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -8,6 +8,8 @@ class GamePlay
   def initialize
     @old_pos = nil
     @new_pos = nil
+    @turn = 'white'
+    @next_turn = 'black'
   end
 
   def set_positions
@@ -27,8 +29,13 @@ class GamePlay
     board[finish[0]][finish[1]].space = " #{board[finish[0]][finish[1]].piece.symbol} ".colorize(background: board[finish[0]][finish[1]].color)
   end
 
+  def switch_turns
+    @turn, @next_turn = @next_turn, @turn
+  end
+
   def play(gameboard)
     loop do
+      puts "--#{@turn}'s turn--"
       gameboard.display_board
       board = gameboard.board
       set_positions
@@ -37,10 +44,12 @@ class GamePlay
       legal = chessman.legal_moves.any?([@new_pos[0], @new_pos[1]])
       chessman.legal_moves.clear unless legal
       redo unless legal
+      redo unless chessman.team == @turn
       move_from(@old_pos, board)
       move_to(chessman, @new_pos, board)
       chessman.legal_moves.clear
       chessman.instance_of?(Pawn) ? chessman.moved_once = true : nil
+      switch_turns
     end
   end 
 end

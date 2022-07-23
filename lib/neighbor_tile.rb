3ea636aefd65fa2bb28e0d9565617cad699011c9
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-22 20:34:38 -0700

    Refactor NeighborTile module and add comments

--- lib/neighbor_tile.rb
+++ lib/neighbor_tile.rb
@@ -1,24 +1,21 @@
 module NeighborTile
-  # Checks if the combined arrays actually represent 
+  # Checks if the combined arrays actually represent
   # an existent coordinate on an 8x8 board
   def in_bounds?(init, adj)
     ((init[0] + adj[0]).between?(1, 8) && (init[1] + adj[1]).between?(1, 8))
   end
-  # Returns the square object that's inside the @board
+  # Returns the adjacent square object
+  # Used in enemy?
   def adj_square(board, init, adj)
     board[init[0] + adj[0]][init[1] + adj[1]] if in_bounds?(init, adj)
   end
-
+  # Check for empty squares
   def no_piece?(square)
     square.nil? || square.piece == ' '
   end
 
-  def same_team?(enemy, ally)
-    enemy != ally
-  end
-
   def enemy?(board, init, adj)
     sqr = adj_square(board, init, adj)
-    same_team?(sqr.piece.team, board[init[0]][init[1]].piece.team) unless no_piece?(sqr)
+    sqr.piece.team != board[init[0]][init[1]].piece.team unless no_piece?(sqr)
   end
 end

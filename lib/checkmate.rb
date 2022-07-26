133ff0db0ed353e701b73ec5602ee42bcc822a8a
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-26 03:03:49 -0700

    Add #filter_legals method to Check, edit #check? and #mate? to clear out a pieces @legals after determining check status

--- lib/checkmate.rb
+++ lib/checkmate.rb
@@ -1,6 +1,23 @@
 require_relative 'deep_dup.rb'
 
 class Check
+  # Remove every array in legals that, when moved to,
+  # doesn't take the king out of check
+  def filter_legals(board, king, piece, init, enemy)
+    filtered_legals = []
+    piece.legals.each do |sqr|
+      # Create a deep copy of the board hash
+      # To peform a legal move without altering the main board
+      fake_board = board.deep_dup
+      fake_board[init[0]][init[1]].piece = ' '
+      fake_board[sqr[0]][sqr[1]].piece = piece
+      # Add legal moves that result in a board where king is no longer in check
+      # to the new filtered array which will replace the pieces @legals array
+      filtered_legals.push(sqr) unless check?(fake_board, king, enemy)
+    end
+    piece.legals.replace(filtered_legals)
+  end
+
   def check?(board, king, enemy)
     check = false
     # Go through the entire board hash's 64 squares
@@ -9,11 +26,12 @@ class Check
         # If the piece on that sqr is an enemy
         next unless square.piece.team == enemy
         # Determine legals which can be check for enemy king
-        square.piece.generate_legals([row, column], board) 
+        square.piece.generate_legals([row, column], board)
         square.piece.legals.each do |sqr|
           # Enemy is in check if any legal squares contain the king
           check = true if board[sqr[0]][sqr[1]].piece == king
         end
+        square.piece.legals.clear
       end
     end
 
@@ -44,6 +62,7 @@ class Check
           # or takes it out of check (pushes a false boolean)
           still_in_check << check?(fake_board, king, enemy)
         end
+        square.piece.legals.clear
       end
     end
     # If all possible legal moves are true, 
--- spec/checkmate_spec.rb
+++ spec/checkmate_spec.rb
@@ -250,11 +250,100 @@ describe Check do
                  8 => Square.new(:light_white) }
         }
         board = chessboard.board
-        print "\n"
-        chessboard.display_board
+        #print "\n"
+        #chessboard.display_board
         king = board[8][8].piece
         result = check.mate?(board, king, 'black', 'white')
         expect(result).to eq(true)
+      end
+    end
+  end
+
+  describe '#filter_legals' do
+    context 'When black king is in check' do
+      it 'Restricts certain legal moves that would otherwise be available' do
+        chessboard.board = {
+          8 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(King.new(:black, 'black'), :light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(Bishop.new(:black, 'black'), :light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          7 => { 1 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 2 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                 3 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          6 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          5 => { 1 => Square.new(Rook.new(:black, 'black'), :light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(Bishop.new(:white, 'white'), :light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          4 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          3 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 8 => Square.new(:light_white) },
+
+          2 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(Pawn.new(:white, 'white'), :light_red) },
+
+          1 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(Rook.new(:white, 'white'), :light_white),
+                 7 => Square.new(King.new(:white, 'white'), :light_red),
+                 8 => Square.new(:light_white) }
+        }
+        board = chessboard.board
+        print "\n"
+        chessboard.display_board
+        king = board[8][3].piece
+        bishop = board[8][5].piece
+        bishop.generate_legals([8, 5], board)
+        check.filter_legals(board, king, bishop, [8, 5], 'white')
+        result = bishop.legals
+        expect(result).to eq([[7, 4]])
       end
     end
   end

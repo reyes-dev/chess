6cbdec2c8ea6e853c6d0a9b631a957ea09486573
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-26 00:32:00 -0700

    Add Check class #mate? method, tests and comment Check class

--- lib/checkmate.rb
+++ lib/checkmate.rb
@@ -1,22 +1,53 @@
+require_relative 'deep_dup.rb'
+
 class Check
-  # Discovers a king is in check by iterating through the board
-  # and generating legals each piece of the enemy team
-  #  any of the legal squares any piece contains the King,
-  # it can be attacked next turn, and is thus in check.
   def check?(board, king, next_turn)
     check = false
-
+    # Go through the entire board hash's 64 squares
     board.each do |row, columns|
       columns.each do |column, square|
+        # If the piece on that sqr is an enemy
         next unless square.piece.team == next_turn
-
-        square.piece.generate_legals([row, column], board)
+        # Determine legals which can be check for enemy king
+        square.piece.generate_legals([row, column], board) 
         square.piece.legals.each do |sqr|
+          # Enemy is in check if any legal squares contain the king
           check = true if board[sqr[0]][sqr[1]].piece == king
         end
       end
     end
 
     check
+  end
+
+  # Loop through each team piece alive on the board and generate legals
+  # Create a separate, fake board to make each possible move on
+  # Check on that board check? still returns true
+  # If it does, meaning every possible legal move can't change
+  # the result of check?, then it is mate
+  def mate?(board, king, turn, next_turn)
+    still_in_check = []
+    # Go through the entire board hash's 64 squares
+    board.each do |row, columns|
+      columns.each do |column, square|
+        # If the piece on that square is a team piece
+        next unless square.piece.team == turn
+        # Generate all the moves it can make, disregarding check for now
+        square.piece.generate_legals([row, column], board)
+        square.piece.legals.each do |sqr|
+          # Create a deep copy of the board hash
+          # to peform legal moves without altering original board
+          fake_board = board.deep_dup
+          fake_board[sqr[0]][sqr[1]].piece = square.piece
+          fake_board[row][column].piece = ' '
+          # Test if peforming a legal move leaves the board in check
+          # or takes it out of check (pushes a false boolean)
+          still_in_check << check?(fake_board, king, next_turn)
+        end
+      end
+    end
+    # If all possible legal moves are true, 
+    # no legal moves can take the king out of check 
+    still_in_check.all?(true)
   end
 end
--- spec/checkmate_spec.rb
+++ spec/checkmate_spec.rb
@@ -172,4 +172,90 @@ describe Check do
       end
     end
   end
+
+  describe '#mate?' do
+    context 'When black king cannot move out of check/is mated' do
+      it 'Returns true' do
+        chessboard.board = {
+          8 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(Rook.new(:white, 'white'), :light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(King.new(:black, 'black'), :light_red) },
+
+          7 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          6 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(King.new(:white, 'white'), :light_red) },
+
+          5 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          4 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          3 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          2 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          1 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) }
+        }
+        board = chessboard.board
+        print "\n"
+        chessboard.display_board
+        king = board[8][8].piece
+        result = check.mate?(board, king, 'black', 'white')
+        expect(result).to eq(true)
+      end
+    end
+  end
 end

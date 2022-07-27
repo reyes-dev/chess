8ad6c5bcbaac3c362488ca4cf345f3d854c6dda9
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-27 04:23:26 -0700

    Add #over_attacked_sqr? Castling method

--- lib/castling.rb
+++ lib/castling.rb
@@ -2,6 +2,32 @@ require_relative 'pieces.rb'
 require_relative 'checkmate.rb'
 
 class Castling < Check
+  def over_attacked_sqr?(board, king_pos, rook_pos, enemy)
+    under_attack = false
+    # Go through the entire board hash's 64 squares
+    board.each do |row, columns|
+      columns.each do |column, square|
+        # If the piece on that sqr is an enemy
+        next unless square.piece.team == enemy
+        # If any of the legal squares match the square king moves over
+        # that square is considered under attack
+        square.piece.generate_legals([row, column], board)
+        square.piece.legals.each do |sqr|
+          # King moves leftwards (lower) towards the rook
+          if king_pos[1] > rook_pos[1]
+            under_attack = true if sqr == [king_pos[0], king_pos[1] - 1]
+          # King moves rightwards (higher) towards the rook
+          elsif king_pos[1] < rook_pos[1]
+            under_attack = true if sqr == [king_pos[0], king_pos[1] + 1]
+          end
+        end
+        square.piece.legals.clear
+      end
+    end
+
+    under_attack
+  end
+
   def squares_empty?(board, king_pos, rook_pos)
     is_empty = []
 
@@ -24,7 +50,7 @@ class Castling < Check
 
   # castleable? makes sure that all the conditions of castling
   # are met before letting a castling be performed
-  def castlable?(board, king, rook, enemy)
-    not_yet_moved(king, rook) && !(check?(board, king, enemy))
+  def castlable?(board, king, rook, king_pos, rook_pos, enemy)
+    squares_empty?(board, king_pos, rook_pos) && not_yet_moved(king, rook) && !(check?(board, king, enemy))
   end
 end
--- spec/castling_spec.rb
+++ spec/castling_spec.rb
@@ -1,16 +1,14 @@
-require '../lib/gameplay.rb'
-require '../lib/castling.rb'
+require '../lib/gameplay'
+require '../lib/castling'
 
 describe Castling do
   let(:chessboard) { Board.new }
   describe '#castle' do
     context 'When conditions are successfully met to allow castling' do
       xit 'Moves the king two squares over' do
-        
       end
 
       xit 'Moves the rook to the square the king stepped over' do
-        
       end
     end
   end
@@ -262,7 +260,7 @@ describe Castling do
         board = chessboard.board
         king = board[1][5].piece
         rook = board[1][8].piece
-        result = king.castlable?(board, king, rook, 'black')
+        result = king.castlable?(board, king, rook, [1, 5], [1, 8], 'black')
         expect(result).to eq(false)
       end
     end
@@ -345,27 +343,177 @@ describe Castling do
         board = chessboard.board
         king = board[1][5].piece
         rook = board[1][8].piece
-        result = king.castlable?(board, king, rook, 'black')
+        result = king.castlable?(board, king, rook, [1, 5], [1, 8], 'black')
         expect(result).to eq(true)
       end
     end
 
-    context 'When the king must move over a square that is attacked by an enemy piece during the castle move' do
-      xit 'Returns false' do
-        
+    describe '#over_attacked_sqr?' do
+      context 'When king attempts move over a square attacked by an enemy piece' do
+        it 'Returns true' do
+          chessboard.board = {
+            8 => { 1 => Square.new(Rook.new(:black, 'black'), :light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(King.new(:black, 'black'), :light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(Rook.new(:black, 'black'), :light_red) },
+  
+            7 => { 1 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   2 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                   3 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                   7 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   8 => Square.new(Pawn.new(:black, 'black'), :light_white) },
+  
+            6 => { 1 => Square.new(:light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(:light_red) },
+  
+            5 => { 1 => Square.new(:light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(:light_white),
+                   7 => Square.new(:light_red),
+                   8 => Square.new(:light_white) },
+  
+            4 => { 1 => Square.new(:light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(:light_red) },
+  
+            3 => { 1 => Square.new(:light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(Rook.new(:black, 'black'), :light_white),
+                   7 => Square.new(:light_red),
+                   8 => Square.new(:light_white) },
+  
+            2 => { 1 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                   2 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                   3 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                   8 => Square.new(Pawn.new(:white, 'white'), :light_red) },
+  
+            1 => { 1 => Square.new(Rook.new(:white, 'white'), :light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(King.new(:white, 'white'), :light_red),
+                   6 => Square.new(:light_white),
+                   7 => Square.new(:light_red),
+                   8 => Square.new(Rook.new(:white, 'white'), :light_white) }
+          }
+          board = chessboard.board
+          king = board[1][5].piece
+          result = king.over_attacked_sqr?(board, [1, 5], [1, 8], 'black')
+          expect(result).to eq(true)
+        end
       end
     end
 
-
-    context 'When the king tries to move to a square that is attacked by an enemy piece/leaves the king in check' do
-      xit 'Returns false' do
-        
-      end
-    end
-
-    context 'When all conditions are met to allow king to perform castling' do
-      xit 'Returns true' do
-        
+    describe '#to_attacked_sqr?' do
+      context 'When the king tries to move to a square that is attacked by an enemy piece/leaves the king in check' do
+        it 'Returns false' do
+          chessboard.board = {
+            8 => { 1 => Square.new(Rook.new(:black, 'black'), :light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(King.new(:black, 'black'), :light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(Rook.new(:black, 'black'), :light_red) },
+  
+            7 => { 1 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   2 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                   3 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                   7 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                   8 => Square.new(Pawn.new(:black, 'black'), :light_white) },
+  
+            6 => { 1 => Square.new(:light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(:light_red) },
+  
+            5 => { 1 => Square.new(:light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(:light_white),
+                   7 => Square.new(:light_red),
+                   8 => Square.new(:light_white) },
+  
+            4 => { 1 => Square.new(:light_white),
+                   2 => Square.new(:light_red),
+                   3 => Square.new(:light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(:light_red) },
+  
+            3 => { 1 => Square.new(:light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(:light_red),
+                   6 => Square.new(:light_white),
+                   7 => Square.new(Rook.new(:black, 'black'), :light_red),
+                   8 => Square.new(:light_white) },
+  
+            2 => { 1 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                   2 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                   3 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                   4 => Square.new(:light_red),
+                   5 => Square.new(:light_white),
+                   6 => Square.new(:light_red),
+                   7 => Square.new(:light_white),
+                   8 => Square.new(Pawn.new(:white, 'white'), :light_red) },
+  
+            1 => { 1 => Square.new(Rook.new(:white, 'white'), :light_red),
+                   2 => Square.new(:light_white),
+                   3 => Square.new(:light_red),
+                   4 => Square.new(:light_white),
+                   5 => Square.new(King.new(:white, 'white'), :light_red),
+                   6 => Square.new(:light_white),
+                   7 => Square.new(:light_red),
+                   8 => Square.new(Rook.new(:white, 'white'), :light_white) }
+          }
+          board = chessboard.board
+          print "\n"
+          chessboard.display_board
+          king = board[1][5].piece
+          result = king.to_attacked_sqr?
+        end
       end
     end
   end

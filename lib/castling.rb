fe9910696a7f0ded9aaf98209a2c03a2cf9779df
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-27 03:24:02 -0700

    Add #squares_empty helper method to Castling class

--- lib/castling.rb
+++ lib/castling.rb
@@ -2,6 +2,22 @@ require_relative 'pieces.rb'
 require_relative 'checkmate.rb'
 
 class Castling < Check
+  def squares_empty?(board, king_pos, rook_pos)
+    is_empty = []
+
+    if king_pos[1] > rook_pos[1]
+      (king_pos[1] - 1).downto(rook_pos[1] + 1) do |i|
+        is_empty << (board[king_pos[0]][i].piece == ' ')
+      end
+    elsif king_pos[1] < rook_pos[1]
+      (king_pos[1] + 1).upto(rook_pos[1] - 1) do |i|
+        is_empty << (board[king_pos[0]][i].piece == ' ')
+      end
+    end
+
+    is_empty.all?(true)
+  end
+
   def not_yet_moved(king, rook)
     king.moved == false && rook.moved == false
   end
--- spec/castling_spec.rb
+++ spec/castling_spec.rb
@@ -17,14 +17,168 @@ describe Castling do
 
   describe '#squares_empty?' do
     context 'When all squares between the rook and king are empty' do
-      xit 'Returns true' do
-        
+      it 'Returns true' do
+        chessboard.board = {
+          8 => { 1 => Square.new(Rook.new(:black, 'black'), :light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(King.new(:black, 'black'), :light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(Rook.new(:black, 'black'), :light_red) },
+
+          7 => { 1 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 2 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                 3 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                 7 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 8 => Square.new(Pawn.new(:black, 'black'), :light_white) },
+
+          6 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          5 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          4 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          3 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          2 => { 1 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 2 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 3 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 7 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 8 => Square.new(Pawn.new(:white, 'white'), :light_red) },
+
+          1 => { 1 => Square.new(Rook.new(:white, 'white'), :light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(King.new(:white, 'white'), :light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(Rook.new(:white, 'white'), :light_white) }
+        }
+        board = chessboard.board
+        king = board[1][5].piece
+        rook = board[1][8].piece
+        result = king.squares_empty?(board, [1, 5], [1, 8])
+        expect(result).to eq(true)
       end
     end
 
     context 'When a square between rook and king is occupied' do
-      xit 'Returns false' do
-        
+      it 'Returns false' do
+        chessboard.board = {
+          8 => { 1 => Square.new(Rook.new(:black, 'black'), :light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(King.new(:black, 'black'), :light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(Rook.new(:black, 'black'), :light_red) },
+
+          7 => { 1 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 2 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                 3 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(Pawn.new(:black, 'black'), :light_white),
+                 7 => Square.new(Pawn.new(:black, 'black'), :light_red),
+                 8 => Square.new(Pawn.new(:black, 'black'), :light_white) },
+
+          6 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          5 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          4 => { 1 => Square.new(:light_white),
+                 2 => Square.new(:light_red),
+                 3 => Square.new(:light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(:light_red),
+                 7 => Square.new(:light_white),
+                 8 => Square.new(:light_red) },
+
+          3 => { 1 => Square.new(:light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(:light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(:light_red),
+                 8 => Square.new(:light_white) },
+
+          2 => { 1 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 2 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 3 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 4 => Square.new(:light_red),
+                 5 => Square.new(:light_white),
+                 6 => Square.new(Pawn.new(:white, 'white'), :light_red),
+                 7 => Square.new(Pawn.new(:white, 'white'), :light_white),
+                 8 => Square.new(Pawn.new(:white, 'white'), :light_red) },
+
+          1 => { 1 => Square.new(Rook.new(:white, 'white'), :light_red),
+                 2 => Square.new(:light_white),
+                 3 => Square.new(:light_red),
+                 4 => Square.new(:light_white),
+                 5 => Square.new(King.new(:white, 'white'), :light_red),
+                 6 => Square.new(:light_white),
+                 7 => Square.new(Knight.new(:white, 'white'), :light_red),
+                 8 => Square.new(Rook.new(:white, 'white'), :light_white) }
+        }
+        board = chessboard.board
+        king = board[1][5].piece
+        rook = board[1][8].piece
+        result = king.squares_empty?(board, [1, 5], [1, 8])
+        expect(result).to eq(false)
       end
     end
   end

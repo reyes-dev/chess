4006cc3dbe5135ccad4b7d0c3d38c81b65ecc8b8
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-26 18:55:51 -0700

    Update main.rb to require checkmate.rb

--- main.rb
+++ main.rb
@@ -2,5 +2,6 @@ require_relative 'lib/board.rb'
 require_relative 'lib/team.rb'
 require_relative 'lib/pieces.rb'
 require_relative 'lib/gameplay.rb'
+require_relative 'lib/checkmate.rb'
 
 GamePlay.new.play(Board.new)
